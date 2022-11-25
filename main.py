bcd81d4d005cf24762d413fc67bf1e3687930f2d
zhouzhibo-byte <17854251030@163.com>
2022-11-25 16:14:53 +0800

    kjadshfoihyoi

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from datetime import datetime
 datetime_obj = datetime.now()
 datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
 print('fds')
-print('ahjgdshEya')
+print('ahjgdshqewewqEya')
 print('4334a343')
 print('jijijijijijiji')
